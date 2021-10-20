1d51e22470bd6d176a6319683831a944feefdf2c
Oran Hartley <oranroha@gmail.com>
2021-10-20 10:45:51 -0400

    init

---
+++ index.js
@@ -0,0 +1,205 @@
+import http from "http";
+import { fork } from "child_process";
+
+var delqueue = [];
+var dbqueue = [];
+var children = {
+    deliver1: new fork('./server/deliver.js', [1]),
+    deliver2: new fork('./server/deliver.js', [2]),
+    db1: new fork('./server/db.js', [3]),
+    db2: new fork('./server/db.js', [4]),
+}
+var free = {
+    deliver1: false,
+    deliver2: false,
+    db1: false,
+    db2: false,
+}
+
+const server = new http.createServer(requestHandler);
+
+function requestHandler(req, res) {
+    console.log(req.method);
+    switch (req.method) {
+        case 'GET':
+            if (free.deliver1 || free.deliver2) {
+                if (free.deliver1) {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8081,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                } else {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8082,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                }
+            } else {
+                if (delqueue.length < 15) {
+                    delqueue.push({ req, res });
+                    execdelQueue();
+                } else {
+                    res.writeHead(429);
+                    res.end()
+                }
+            }
+            break;
+        case 'POST':
+            if (free.db1 || free.db2) {
+                if (free.db1) {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8083,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                } else {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8084,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                }
+            } else {
+                if (dbqueue.length < 15) {
+                    dbqueue.push({ req, res });
+                    execdbQueue();
+                } else {
+                    res.writeHead(429);
+                    res.end()
+                }
+            }
+            break;
+        case 'DELETE':
+            if (free.db1 || free.db2) {
+                if (free.db1) {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8083,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                } else {
+                    var connector = http.request({
+                        host: 'localhost',
+                        port: 8083,
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                }
+            } else {
+                if (dbqueue.length < 15) {
+                    dbqueue.push({ req, res });
+                    execdbQueue();
+                } else {
+                    res.writeHead(429);
+                    res.end()
+                }
+            }
+            break;
+        case 'PUT':
+            if (free.db1 || free.db2) {
+                if (free.db1) {
+                    var connector = http.request({
+                        host: 'localhost:8083',
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                } else {
+                    var connector = http.request({
+                        host: 'localhost:8084',
+                        path: req.url,
+                        method: req.method,
+                        headers: req.headers
+                    }, (resp) => {
+                        resp.pipe(res);
+                    });
+                    req.pipe(connector);
+                }
+            } else {
+                if (dbqueue.length < 15) {
+                    dbqueue.push({ req, res });
+                    execdbQueue();
+                } else {
+                    res.writeHead(429);
+                    res.end()
+                }
+            }
+            break;
+        default:
+            res.writeHead(400);
+            res.end();
+            break;
+    }
+}
+
+Object.keys(children).forEach((child, index) => {
+    children[child].on("message", (message) => {
+        if (message == "done") {
+            free[child] = true;
+        } else {
+            free[child] = false;
+        }
+    });
+});
+
+server.listen(80, 'localhost');
+
+function execdelQueue() {
+    (async () => {
+        while (true) {
+            if (free.deliver1 || free.deliver2) {
+                var req = delqueue[0].req;
+                var res = delqueue[0].res;
+                requestHandler(req, res);
+            }
+            await new Promise(r => setTimeout(r, 100));
+        }
+    });
+}
+
+function execdbQueue() {
+    (async () => {
+        while (true) {
+            if (free.db1 || free.db2) {
+                var req = delqueue[0].req;
+                var res = delqueue[0].res;
+                requestHandler(req, res);
+            }
+            await new Promise(r => setTimeout(r, 100));
+        }
+    });
+}
---
+++ server/deliver.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import http from "http";
+import mime from "mime-types";
+
+console.log(process.argv);
+var port = 8080 + parseInt(process.argv[2]);
+
+const server = new http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    console.log(req.url);
+    var response;
+    var err;
+    if (fs.existsSync(req.url != "/" && `./server/webpage${req.url}`) || fs.existsSync(`./server/webpage${req.url}.html`)) {
+        if (fs.existsSync(`./server/webpage${req.url}.html`)) {
+            response = fs.readFileSync(`./server/webpage${req.url}.html`);
+            err = 200;
+        } else {
+            response = fs.readFileSync(`./server/webpage${req.url}`);
+            err = 200;
+        }
+    } else {
+        if (req.url == "/") {
+            response = fs.readFileSync(`./server/webpage/index${process.argv[2]}.html`);
+            err = 200;
+        } else {
+            if (fs.existsSync(`./server/webpage/404.html`)) {
+                response = fs.readFileSync(`./server/webpage/404.html`);
+                err = 404;
+            } else {
+                response = "<h1>The dev was too lazy to add a 404 message, so here we are.</h1>";
+                err = 404;
+            }
+        }
+    }
+    const type = mime.lookup(response);
+    res.writeHead(err, {
+        'Content-Type': type,
+    });
+    res.end(response);
+}
+
+server.listen(port, 'localhost', () => {
+    console.log(`Listenon on localhost ${port}`);
+    process.send('done');
+});
